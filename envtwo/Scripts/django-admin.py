6bf41ddfa48840b47ad6a56674828e957b317519
lesliechiri <dianawantes@gmail.com>
2019-05-15 15:02:57 +0300

    adding files

---
+++ envtwo/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\student\desktop\leslie python\django_projects\project2\envtwo\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
